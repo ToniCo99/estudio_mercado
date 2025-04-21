800634ff42bb97c472d42235120314db74c450e3
ToniCo99 <antoniocoru99@gmail.com>
2025-04-21 17:55:45 +0200

    +autores y correcion errores

--- script.js
+++ script.js
@@ -36,6 +36,58 @@ document.addEventListener('DOMContentLoaded', () => {
         switchTab(firstTabButton.getAttribute('data-tab'));
     }
 
+    // --- Código para el botón de Pantalla Completa del PDF ---
+    const fullscreenBtn = document.getElementById('fullscreen-btn');
+    const pdfFrame = document.getElementById('pdf-frame');
+    const pdfContainer = document.querySelector('.pdf-container');
+
+    if (fullscreenBtn && pdfFrame) {
+        fullscreenBtn.addEventListener('click', () => {
+            if (!document.fullscreenElement) {
+                // Si no estamos en modo pantalla completa, entramos en él
+                if (pdfContainer.requestFullscreen) {
+                    pdfContainer.requestFullscreen();
+                } else if (pdfContainer.webkitRequestFullscreen) { /* Safari */
+                    pdfContainer.webkitRequestFullscreen();
+                } else if (pdfContainer.msRequestFullscreen) { /* IE11 */
+                    pdfContainer.msRequestFullscreen();
+                }
+            } else {
+                // Si ya estamos en pantalla completa, salimos
+                if (document.exitFullscreen) {
+                    document.exitFullscreen();
+                } else if (document.webkitExitFullscreen) { /* Safari */
+                    document.webkitExitFullscreen();
+                } else if (document.msExitFullscreen) { /* IE11 */
+                    document.msExitFullscreen();
+                }
+            }
+        });
+
+        // Actualizar el ícono cuando cambie el estado de pantalla completa
+        document.addEventListener('fullscreenchange', updateFullscreenButton);
+        document.addEventListener('webkitfullscreenchange', updateFullscreenButton);
+        document.addEventListener('msfullscreenchange', updateFullscreenButton);
+
+        function updateFullscreenButton() {
+            if (document.fullscreenElement) {
+                fullscreenBtn.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+                    <path d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z"/>
+                </svg>
+                `;
+            } else {
+                fullscreenBtn.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+                    <path d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/>
+                </svg>
+                `;
+            }
+        }
+    }
+
     // --- Código para Gráficos Chart.js ---
 
     // Datos y configuración Gráfico 1: Comparativa Nutricional (Proteínas)
